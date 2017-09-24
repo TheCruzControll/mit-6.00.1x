4c1efd69b4ba615ad41b1d4ceefa441ba861e0df
Francis N. Cruz <francisnikocruz@gmail.com>
2017-09-23 23:38:08 -0700

    Deleted the subsection folder

---
+++ week-3/apply-to-each/apply-to-each.py
@@ -0,0 +1,11 @@
+def absoluteValue(x):
+    return abs(x)
+applyToEach(testList,absoluteValue)
+
+def plusOne(x):
+    return x+1
+applyToEach(testList,plusOne)
+
+def square(x):
+    return x**2
+applyToEach(testList,square)
