dd98731d9c816be9ab30d22138cd7ef48c299ce5
Francis N. Cruz <francisnikocruz@gmail.com>
2017-09-25 23:03:12 -0700

    Added how many exercise

---
+++ week-3/how-many/how-many.py
@@ -0,0 +1,11 @@
+def how_many(aDict):
+    '''
+    aDict: A dictionary, where all the values are lists.
+
+    returns: int, how many values are in the dictionary.
+    '''
+    # Your Code Here
+    result = 0
+    for v in aDict.values():
+        result += len(v)
+    return result
