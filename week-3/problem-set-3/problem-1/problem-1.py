62af9c6bf741395ad560d5dabbf279356d4f01b1
Francis N. Cruz <francisnikocruz@gmail.com>
2017-09-26 20:20:02 -0700

    Added Problem 1

---
+++ week-3/problem-set-3/problem-1/problem-1.py
@@ -0,0 +1,14 @@
+def isWordGuessed(secretWord, lettersGuessed):
+    '''
+    secretWord: string, the word the user is guessing
+    lettersGuessed: list, what letters have been guessed so far
+    returns: boolean, True if all the letters of secretWord are in lettersGuessed;
+      False otherwise
+    '''
+    # FILL IN YOUR CODE HERE...
+    for i in list(secretWord):
+        if i in lettersGuessed:
+            continue
+        else:
+            return False
+    return True
