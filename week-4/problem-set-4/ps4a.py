c3a36903d745ac6b1909d2c0ec5599471fa0cb79
Francis N. Cruz <francisnikocruz@gmail.com>
2017-10-01 00:53:30 -0700

    Edited ps4a

--- week-4/problem-set-4/ps4a.py
+++ week-4/problem-set-4/ps4a.py
@@ -157,10 +157,10 @@ def updateHand(hand, word):
     """
     # TO DO ... <-- Remove this comment when you code this function
 
-    #Make a copy of the hand so you dont mutate it. 
+    # Make a copy of the hand so you dont mutate it.
     handCopy = dict(hand)
     
-    #Go through each letter in the word. If the letter is in your hand, take off 1 value.
+    # Go through each letter in the word. If the letter is in your hand, take off 1 value.
     for letter in word:
         if letter in handCopy.keys():
             handCopy[letter] -=1
@@ -175,13 +175,29 @@ def isValidWord(word, hand, wordList):
     composed of letters in the hand. Otherwise, returns False.
 
     Does not mutate hand or wordList.
-   
+
     word: string
     hand: dictionary (string -> int)
     wordList: list of lowercase strings
     """
     # TO DO ... <-- Remove this comment when you code this function
+    handCopy = dict(hand)
 
+    if word in wordList:
+        for letter in word:
+            # If the Letter is not in the hand, return False
+            if letter not in hand.keys():
+                return False
+            # If there is no more of that letter in the Hand, return False
+            if handCopy[letter] == 0:
+                return False
+            # Takes the number of letters in the hand down by one
+            handCopy[letter] -= 1
+        # If all of this is okay return True
+        return True
+    # If the word isn't valid, return False
+    else:
+        return False
 
 #
 # Problem #4: Playing a hand
